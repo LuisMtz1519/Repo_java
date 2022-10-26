832603869ab6d4f239d368d624a5dcd484a781a7
LuisMtz1899692 <luisalonso20101@hotmail.com>
2022-10-25 20:43:07 -0500

    Se corrigieron lineas al codigo html

--- js5.js
+++ js5.js
@@ -1,36 +1,38 @@
-function operaciones(numero,numero2,operacion){
-    let resultado=0;
+function operaciones(num,num2,operacion){
+    let resultados=0;
     switch(operacion){
         case '+':
-            resultado = numero + numero2;
+            resultados=num+num2;
             break;
         case '-':
-            resultado = numero - numero2;
+            resultados=num-num2;
             break;
         case '/':
-            resultado = numero / numero2;
+            resultados=num/num2;
             break;
         case '*':
-            resultado = numero * numero2;
+            resultados=num*num2;
             break;
     }
-    return resultado
+    return resultados;
 }
 
-function calcular(){
+function calcularop(){
     let numero = 
     parseInt(document.getElementById("numero").value);
-    console.log(numero)
+    console.log(numero);
+
     let numero2 = 
     parseInt(document.getElementById("numero2").value);
-    console.log(numero2)
+    console.log(numero2);
 
     let operacion=
-    document.getElementById("operacion")
+    document.getElementById("operacion").value;
+    console.log(operacion);
+    console.log(typeof operacion);
 
     let resultado=
     document.getElementById("result");
-
-    resultado.value=operaciones(numero,numero2,operacion)
-    console.log(resultado)
+    resultado.value=operaciones(numero,numero2,operacion);
+    console.log(resultado);
 };
